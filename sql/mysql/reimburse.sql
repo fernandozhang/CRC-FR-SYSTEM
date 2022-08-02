888c59dca9f7b0ad032bd182d50753313c169f2e
fernandozhang <fernandozhang@126.com>
2022-08-02 17:44:02 +0800

    新增批次打印表和关联报销记录表

--- sql/mysql/reimburse.sql
+++ sql/mysql/reimburse.sql
@@ -40,3 +40,30 @@ CREATE TABLE `ruoyi-vue-pro`.`bpm_reim_purchase` (
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
 COMMENT='购物报销表';
+
+CREATE TABLE `ruoyi-vue-pro`.`bpm_reim_print_batch` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT COMMENT '报销打印批次表主键',
+  `amount` DOUBLE NOT NULL COMMENT '总金额',
+  `staff_name` VARCHAR(256) NOT NULL COMMENT '报销员工姓名',
+  `reim_type` INT NOT NULL COMMENT '报销类型',
+  `create_time` DATETIME NOT NULL COMMENT '创建时间',
+  `creator` VARCHAR(64) NOT NULL COMMENT '创建者',
+  `update_time` DATETIME COMMENT '更新时间',
+  `updater` VARCHAR(64) COMMENT '更新者',
+  `deleted` BIT NOT NULL COMMENT '是否删除',
+  `tenant_id` BIGINT NOT NULL COMMENT '租户编号',
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+COMMENT='报销批量打印表';
+
+CREATE TABLE `bpm_reim_print_batch_relate_id` (
+  `id` bigint(20) NOT NULL COMMENT '报销批量打印关联表主键',
+  `batch_id` bigint(20) NOT NULL COMMENT '报销批量打印表编号',
+  `relate_id` bigint(20) NOT NULL COMMENT '关联报销记录编号',
+  `creator` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '创建者',
+  `creat_time` datetime NOT NULL COMMENT '创建时间',
+  `updater` varchar(64) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '更新者',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  `deleted` bit(1) NOT NULL COMMENT '是否删除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='报销批次打印关联报销记录表';
